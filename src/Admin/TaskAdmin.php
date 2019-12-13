0e17f9dbb916d7a1453b073217a2911b77ae8027
Abel <abelviqueira@gmail.com>
2019-12-13 11:28:14 +0100

    Listado de tareas muestra el usuario asignado

--- src/Admin/TaskAdmin.php
+++ src/Admin/TaskAdmin.php
@@ -39,7 +39,7 @@ final class TaskAdmin extends AbstractAdmin {
     {
         $listMapper
             ->addIdentifier('name')
-            ->addIdentifier('id');
+            ->addIdentifier('assigned.email');
     }
 
     public function toString($object)
